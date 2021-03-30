20330fc9b9180e0c6a80f9b0682c0409eaf9b3c6
klueless-io <david@ideasmen.com.au>
2021-03-30 19:15:10 +1100

    final project

--- src/components/NameInput.js
+++ src/components/NameInput.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export function NameInput(props) {
